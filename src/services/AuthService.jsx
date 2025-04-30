0a812fe5cdd917d15edfe2b98e708ac265abf303
adrianccrs <yeadcato@gmail.com>
2025-04-30 11:38:40 -0400

    AuthService created

---
+++ src/services/AuthService.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+const api = "http://localhost:8080/api";
+import { handleError } from "../helpers/ErrorHandler";
+
+export const loginAPI = async (email, password) => {
+    try {
+        const data = await axios.post(api + "/auth/login", {
+            email: email,
+            password: password,
+        });
+        return data;
+    } catch (error){
+        handleError(error);
+    } 
+}
+
+export const registerAPI = async (firstName, lastName, email, password, program, description) => {
+    try {
+        const data = await axios.post(api + "/auth/register", {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+            program: program,
+            description: description
+        });
+        return data;
+    } catch (error){
+        handleError(error);
+    }
+}
