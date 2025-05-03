42157f8e88a879042bb2b46bcd936ec7436da698
adrianccrs <yeadcato@gmail.com>
2025-05-03 18:30:35 -0400

    Project properties names changed

--- src/components/YourProjects.jsx
+++ src/components/YourProjects.jsx
@@ -4,8 +4,8 @@ export default function YourProjects({yourProjects = []}) {
         <div  className="flex flex-wrap flex-col justify-start bg-stone-950 gap-4 w-2/3 h-full max-w-md border-r border-divider p-6 overflow-y-auto">
         <h2 className="font-manrope text-xl text-green-600">Tus proyectos</h2>
             {yourProjects.map((project) => (
-                <Link className="hover:text-green-500 text-sm" color="foreground" href={project.href} underline="hover">
-                    {project.group + " / " + project.name}
+                <Link className="hover:text-green-500 text-sm" color="foreground" href={project.repoLink} underline="hover">
+                    {project.groupId + " / " + project.title}
                 </Link>
             ))}
         </div>
