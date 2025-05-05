ce1f70cc1ac10a7f5ab6c38d5c974dfb0df3a8a9
adrianccrs <yeadcato@gmail.com>
2025-05-05 01:35:18 -0400

    Interceptors for add the token and baseURL changed

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,27 +1,40 @@
-import axios from "axios";
-
-
+import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "http://localhost:8080/api",
-    headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${localStorage.getItem("token")}`,
-    },
+  baseURL: "http://localhost:8080",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+api.interceptors.request.use(
+  (config) => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+      } else {
+        console.log("No token found");
+      }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 
 api.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        if (error.response.status === 401) {
-            localStorage.removeItem("token");
-            window.location.href = "/login";
-        }
-        return Promise.reject(error);
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401 || error.response.status === 403) {
+      localStorage.removeItem("token");  
+      localStorage.removeItem("userId"); 
+      window.location.href = "/login"; 
     }
+    return Promise.reject(error);
+  }
 );
 
-export const apiUrl = "http://localhost:8080/api";
+export const apiUrl = "http://localhost:8080";
