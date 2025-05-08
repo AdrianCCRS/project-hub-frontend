01464ac014ceec7c3f4b8c1bfe413660f0d9f050
adrianccrs <yeadcato@gmail.com>
2025-05-08 18:27:38 -0400

    PlusIcon added

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -443,4 +443,27 @@ export const SearchIcon = (props) => {
       </svg>
     );
 };
+
+export const CancelIcon = ({fill = "currentColor", size, height, width, ...props}) => {
+    return (
+        <svg 
+            width={size || width} 
+            height={size || height} 
+            viewBox="0 0 512 512" 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            {...props}
+        >
+            <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                <g id="work-case" fill={fill} transform="translate(91.520000, 91.520000)">
+                    <polygon 
+                        id="Close" 
+                        points="328.96 30.2933333 298.666667 0 164.48 134.4 30.2933333 0 0 30.2933333 134.4 164.48 0 298.666667 30.2933333 328.96 164.48 194.56 298.666667 328.96 328.96 298.666667 194.56 164.48"
+                    />
+                </g>
+            </g>
+        </svg>
+    );
+};
+
   
