6d18ea122292f57918828f300f421971ce7a2ddd
adrianccrs <yeadcato@gmail.com>
2025-05-07 23:36:41 -0400

    PlusIcon and SearchIcon added

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -219,7 +219,7 @@ export const LogoutIcon = ({fill = "currentColor", size, height, width, ...props
     );
 };
 
-export const ChevronDown = ({fill, size, height, width, ...props}) => {
+export const ChevronDown = ({fill = "currentColor", size, height, width, ...props}) => {
   return (
     <svg
       fill="none"
@@ -385,6 +385,62 @@ export const EditIcon = ({fill = "currentColor", size, height, width, ...props})
                 strokeWidth={1.5}
             />
         </svg>
+    );
+};
+
+export const PlusIcon = ({size = 24, width, height, ...props}) => {
+    return (
+      <svg
+        aria-hidden="true"
+        fill="none"
+        focusable="false"
+        height={size || height}
+        role="presentation"
+        viewBox="0 0 24 24"
+        width={size || width}
+        {...props}
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={1.5}
+        >
+          <path d="M6 12h12" />
+          <path d="M12 18V6" />
+        </g>
+      </svg>
+    );
+};
+
+export const SearchIcon = (props) => {
+    return (
+      <svg
+        aria-hidden="true"
+        fill="none"
+        focusable="false"
+        height="1em"
+        role="presentation"
+        viewBox="0 0 24 24"
+        width="1em"
+        {...props}
+      >
+        <path
+          d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+        />
+        <path
+          d="M22 22L20 20"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+        />
+      </svg>
     );
 };
   
