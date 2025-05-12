fb3376e797fd49dccf14fee0bc062bd10b853a73
adrianccrs <yeadcato@gmail.com>
2025-05-12 16:31:37 -0400

    Methods for updating groups

--- src/services/GroupService.jsx
+++ src/services/GroupService.jsx
@@ -3,7 +3,7 @@ import {api} from "./axiosConfig";
 
 export const createGroupAPI = async (name, leaderId) => {
     try {
-        const data = await api.post("/api/group", {
+        const data = await api.post("/api/groups", {
             name: name,
             leaderId: leaderId,
         });
@@ -13,11 +13,40 @@ export const createGroupAPI = async (name, leaderId) => {
     } 
 }
 
-export const addMembers = async (members) => {
+export const addMembersAPI = async (members) => {
     try {
-        const data = await api.post("/api/group-members/addMultiple", {
-            members: members,
+        const data = await api.post("/api/group-members/addMultiple", members);
+        return data;
+    } catch (error){
+        handleError(error);
+    }
+}
+
+export const updateGroupAPI = async (name, leaderId, id) => {
+    try {
+        const data = await api.put("/api/groups", {
+            name: name,
+            leaderId: leaderId,
+            id: id
         });
+        return data;
+    } catch (error){
+        handleError(error);
+    }
+}
+
+export const getGroupsByLeaderAPI = async (leaderId) => {
+    try {
+        const data = await api.get(`/api/groups/editable/${leaderId}`);
+        return data;
+    } catch (error){
+        handleError(error);
+    }
+}
+
+export const getMembersFromGroupAPI = async (groupId) => {
+    try {
+        const data = await api.get(`/api/group-members/group/${groupId}`);
         return data;
     } catch (error){
         handleError(error);
